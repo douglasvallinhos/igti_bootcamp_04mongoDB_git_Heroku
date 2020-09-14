052a0f3f3720624804dc29783b2e36da2fdcea1b
Douglas Vallinhos <douglas.m.vallinhos@gmail.com>
2020-09-14 19:10:24 -0300

    aula7HEROKU

---
+++ aula07_HerokuAPP/models/studentModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const studentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  subject: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    require: true,
+  },
+  value: {
+    type: Number,
+    require: true,
+    min: 0,
+    validate: (value) => {
+      if (value < 0) {
+        throw new Error('Valor negativo não permitido');
+      }
+    },
+  },
+  lastModified: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const studentModel = mongoose.model('student', studentSchema, 'student');
+
+export { studentModel };
