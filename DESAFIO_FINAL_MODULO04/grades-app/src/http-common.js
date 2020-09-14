03620c8a01e829da5825ecfe8195c012b9a22bcc
Douglas Vallinhos <douglas.m.vallinhos@gmail.com>
2020-09-14 19:29:22 -0300

    desafio final modulo4

---
+++ DESAFIO_FINAL_MODULO04/grades-app/src/http-common.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+//Define a URL base da origem para consumo do servico
+export default axios.create({
+  baseURL:
+    process.env.REACT_APP_PORT_API === ''
+      ? process.env.REACT_APP_HOST_API
+      : `${process.env.REACT_APP_HOST_API}:${process.env.REACT_APP_PORT_API}`,
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
